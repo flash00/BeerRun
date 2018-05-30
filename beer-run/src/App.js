9fda3e638de6af24bbca29a2512f3e58c42cb206
flash00 <dexlorraine@gmail.com>
2018-05-29 18:55:37 -0700

    Add secret key and App

--- beer-run/src/App.js
+++ beer-run/src/App.js
@@ -3,17 +3,18 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  constuctor(){
+    super();
+
+  }
+
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <Delivery />
       </div>
+
+
     );
   }
 }
